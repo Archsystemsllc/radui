f2552941a2c087a1baf74b794567cbdeb411cf28
Abdul Nissar <ashaik@archsystemsinc.com>
2018-04-06 00:05:59 -0400

    Production Deployment 1

--- src/main/java/com/archsystemsinc/rad/configuration/WebMvcConfig.java
+++ src/main/java/com/archsystemsinc/rad/configuration/WebMvcConfig.java
@@ -38,7 +38,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	@Bean
    	public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
 		///Local Development Environment
-    	String activeProfile = System.getProperty("spring.profiles.active",	"local");
+    	//String activeProfile = System.getProperty("spring.profiles.active",	"local");
     	
     	//AWS Development Environment
     	//String activeProfile = System.getProperty("spring.profiles.active",	"development");
@@ -47,7 +47,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     	//String activeProfile = System.getProperty("spring.profiles.active", "uat");
     	
     	//AWS Prod Environment
-    	//String activeProfile = System.getProperty("spring.profiles.active", "prod");
+    	String activeProfile = System.getProperty("spring.profiles.active", "prod");
     	
    		String propertiesFilename = "application-" + activeProfile	+ ".properties";
    		System.out.println("propertiesFilename:" + propertiesFilename);
